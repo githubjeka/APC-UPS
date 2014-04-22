dfc1308fa6b6a9c0d883b8747a1f5d0b6f13027f
githubjeka <et.coder@gmail.com>
2014-04-22 16:47:16 +0300

    init

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HidLibrary;
+
+namespace ApcUps
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            Task upsTask = Task.Factory.StartNew( () => {                               
+                               
+                HidDevice ups = HidDevices.Enumerate(0x051D, 0x0002).FirstOrDefault();
+
+                if (ups == null) return;
+
+                do
+                {                 
+                    byte[] status;
+                    byte[] status2;
+                    byte[] status3;
+
+                    ups.ReadFeatureData(out status, 0x06);
+                    ups.ReadFeatureData(out status2, 0x31);
+                    ups.ReadFeatureData(out status3, 0x50);
+
+                    FileWriter.addValue(status);
+                    FileWriter.addValue(status2);
+                    FileWriter.addValue(status3);                    
+
+                    Thread.Sleep(4000);
+
+                } while (true);
+            });
+
+            upsTask.Wait();
+            upsTask.Dispose();
+            
+        }
+    }
+}
---
+++ ConsoleApplication1/FileWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ApcUps
+{
+    class FileWriter
+    {
+        static string path = "D:/WriteText.txt";     
+                
+        public static void addValue(byte[] value)
+        {
+            File.AppendAllText(path, BitConverter.ToString(value) + "\n", Encoding.UTF8);            
+        }
+    }
+}
